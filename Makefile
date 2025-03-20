6728d78f813872c670083cc3a461dc5d90cfa5d9
mathieubest <99485247+mathieubest@users.noreply.github.com>
2025-03-20 20:58:57 +0100

    pushing to github

---
+++ Makefile
@@ -0,0 +1,7 @@
+push:
+	git add .
+	git commit -m "pushing to github"
+	git push
+
+pull:
+	git pull
--- Pump-Progress-App/Pump-Progress-App/LandingView.swift
+++ Pump-Progress-App/Pump-Progress-App/LandingView.swift
@@ -3,18 +3,26 @@ import SwiftUI
 struct LandingView: View {
     var body: some View {
         VStack {
+            Image("pumpLogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .padding(.top, 80)
+            
             // Spacer()
 
-            Text("Pump Progress")
+            Text("Pump Progress".uppercased())
                 .font(.largeTitle)
                 .fontWeight(.bold)
-                .padding(.vertical, 250)
+                .padding(.vertical, 50)
 
             Text("Track your lifts, monitor your gains and push your limits!")
                 .font(.body)
+                .fontWeight(.semibold)
                 .multilineTextAlignment(.center)
                 .padding(.horizontal, 30)
 
+            Spacer()
             Button(action: {
                 // TODO: Add login/signup action
             }) {
@@ -26,7 +34,7 @@ struct LandingView: View {
                     .background(Color.blue)
                     .cornerRadius(10)
             }
-            .padding(.bottom, 50)
+            .padding(.bottom, 250)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.systemGray6))
